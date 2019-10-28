ea896f06111fc8d0978a2844fcf99ba13951bc5a
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-10-28 13:00:44 +0300

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
-
 import "./App.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <h1>hello</h1>;
+    return (
+      <Router>
+        <main>
+          {/* navbar */}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
   }
 }
 
