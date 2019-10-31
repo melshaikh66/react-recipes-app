cc61449b02619c564c0288566137e6adb5726ce9
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-10-31 22:30:50 +0300

    recipes list

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import { recipeData } from "../data/tempDetails";
+import { Link } from "react-router-dom";
 
 export default class SingleRecipe extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
   render() {
     return <h4>Hello from single recipe page</h4>;
   }
